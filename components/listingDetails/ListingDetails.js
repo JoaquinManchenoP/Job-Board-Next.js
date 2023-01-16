abd7cbef0a92c9598182d75f96a445e87f48ca77
JoaquinManchenoP <manchenojj@g.cofc.edu>
2023-01-16 13:22:34 -0500

    dynamic listing page

--- components/listingDetails/ListingDetails.js
+++ components/listingDetails/ListingDetails.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import { MdWork } from 'react-icons/md';
+import { BiTime } from 'react-icons/bi';
+import { BsFillFilePersonFill } from 'react-icons/bs';
+import { BsCash } from 'react-icons/bs';
 
 export default function ListingDetails() {
     return (
-        <div className="details h-[500px] w-10/12 bg-orange-500">
-
+        <div className="details h-[500px] w-10/12 bg-orange-500 space-y-4">
+            <div className="employer h-8 w-full bg-red-400 flex items-center space-x-3">
+                <div className="employer__icon">
+                    <MdWork className='text-2xl' />
+                </div>
+                <dic className="employer__name">
+                    Employer name
+                </dic>
+            </div>
+            <div className="time h-8 w-full bg-red-400 flex items-center space-x-3">
+                <div className="time__icon">
+                    <BiTime className='text-2xl' />
+                </div>
+                <div className="time__text">
+                    Full time
+                </div>
+            </div>
+            <div className="aplicants h-8 w-full bg-red-400 flex items-center space-x-3">
+                <div className="aplicants__icon">
+                    <BsFillFilePersonFill className='text-2xl' />
+                </div>
+                <div className="number__aplicants">
+                    6 Aplicants
+                </div>
+            </div>
+            <div className="selected__skills">
+                <div>
+                    react
+                </div>
+                <div>
+                    svelte
+                </div>
+            </div>
+            <div className="salary h-8 w-full bg-red-400 flex items-center space-x-3">
+                <div className="salary__icon">
+                    <BsCash className='text-2xl' />
+                </div>
+                <div className="salary">
+                    100,000 - 200,000
+                </div>
+            </div>
         </div>
 
     )
--- pages/listings/[ListingId].js
+++ pages/listings/[ListingId].js
@@ -5,10 +5,13 @@ export default function Listing() {
     return (
         <div className="listing h-screen w-screen bg-purple-500 flex justify-center">
             <div className="listing__container h-full w-11/12 flex">
-                <div className="right h-full w-2/6 bg-green-400 flex justify-center pt-8">
+                <div className="right h-full w-1/4 bg-green-400 flex flex-col items-center space-y-3 ">
+                    <div className="back__button">
+                        back button
+                    </div>
                     <ListingDetails />
                 </div>
-                <div className="left h-full w-4/6 bg-orange-500">
+                <div className="left h-full w-3/4 bg-orange-500">
                     left_side
                 </div>
             </div>
