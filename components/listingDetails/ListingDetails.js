2c2bd14335842f39637f1435d6b361fd981f9d09
JoaquinManchenoP <manchenojj@g.cofc.edu>
2023-01-26 19:20:35 -0500

    responsive

--- components/listingDetails/ListingDetails.js
+++ components/listingDetails/ListingDetails.js
@@ -3,10 +3,12 @@ import { MdWork } from 'react-icons/md';
 import { BiTime } from 'react-icons/bi';
 import { BsFillFilePersonFill } from 'react-icons/bs';
 import { BsCash } from 'react-icons/bs';
+import SkillTag from '../jobCard/SkillTag';
+
 
 export default function ListingDetails() {
     return (
-        <div className="details h-[500px] w-10/12 bg-orange-500 space-y-4">
+        <div className="details h-full w-full bg-orange-500 space-y-4">
             <div className="employer h-8 w-full bg-red-400 flex items-center space-x-3">
                 <div className="employer__icon">
                     <MdWork className='text-2xl' />
@@ -31,12 +33,12 @@ export default function ListingDetails() {
                     6 Aplicants
                 </div>
             </div>
-            <div className="selected__skills">
+            <div className="selected__skills space-y-3">
                 <div>
-                    react
+                    <SkillTag tag={"react"} />
                 </div>
                 <div>
-                    svelte
+                    <SkillTag tag={"svelte"} />
                 </div>
             </div>
             <div className="salary h-8 w-full bg-red-400 flex items-center space-x-3">
--- pages/listings/[ListingId].js
+++ pages/listings/[ListingId].js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ListingDetails from '../../components/listingDetails/ListingDetails'
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { useRouter } from 'next/router';
+
 
 export default function Listing() {
+    const router = useRouter();
     return (
-        <div className="listing h-screen w-screen bg-purple-500 flex justify-center">
-            <div className="listing__container h-full w-11/12 flex">
-                <div className="right h-full w-1/4 bg-green-400 flex flex-col items-center space-y-3 ">
-                    <div className="back__button">
-                        back button
+        <div className="listing h-screen w-screen flex justify-center">
+            <div className="listing__container h-full w-11/12 md:flex xl:flex">
+                <div className="right h-full md:-full lg:w-1/4 xl:w-1/4 bg-green-400 flex flex-col items-center space-y-4 ">
+                    <div className="back__button pt-5">
+                        <button onClick={() => router.back()}>
+                            <AiOutlineArrowLeft className='text-3xl' />
+                        </button>
                     </div>
-                    <ListingDetails />
+                    <div className='listing__details h-[500px] w-10/12'>
+                        <ListingDetails />
+                    </div>
                 </div>
                 <div className="left h-full w-3/4 bg-orange-500">
                     left_side
