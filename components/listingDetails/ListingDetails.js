662d2f1b081f1b732e32aea70d880fc0a11b2084
JoaquinManchenoP <manchenojj@g.cofc.edu>
2023-01-31 17:30:43 -0500

    m

--- components/listingDetails/ListingDetails.js
+++ components/listingDetails/ListingDetails.js
@@ -3,47 +3,47 @@ import { MdWork } from 'react-icons/md';
 import { BiTime } from 'react-icons/bi';
 import { BsFillFilePersonFill } from 'react-icons/bs';
 import { BsCash } from 'react-icons/bs';
-import { CgWebsite } from 'react-icons/cg';
+import { CgFontSpacing, CgWebsite } from 'react-icons/cg';
 
 
 
-export default function ListingDetails() {
+export default function ListingDetails({ details }) {
     return (
         <div className="details h-full w-full space-y-4">
-            <div className="employer h-8 w-full flex items-center space-x-3">
+            <div className="employer h-8 w-full flex items-center space-x-3 min-w-max">
                 <div className="employer__icon">
                     <MdWork className='text-2xl' />
                 </div>
-                <dic className="employer__name">
-                    Employer name
-                </dic>
+                <div className="employer__name ">
+                    <span>{details.employerName}</span>
+                </div>
             </div>
-            <div className="time h-8 w-full flex items-center space-x-3">
+            <div className="time h-8 w-full flex items-center space-x-3 min-w-max">
                 <div className="time__icon">
                     <BiTime className='text-2xl' />
                 </div>
                 <div className="time__text">
-                    Full time
+                    <span>{details.jobTime}</span>
                 </div>
             </div>
-            <div className="aplicants h-8 w-full flex items-center space-x-3">
+            <div className="aplicants h-8 w-full flex items-center space-x-3 min-w-max">
                 <div className="aplicants__icon">
                     <BsFillFilePersonFill className='text-2xl' />
                 </div>
-                <div className="number__aplicants">
-                    6 Aplicants
+                <div className="jobType">
+                    <span>{details.jobType}</span>
                 </div>
             </div>
-            <div className="companu__website h-8 w-full flex items-center space-x-3">
+            <div className="companu__website h-8 w-full flex items-center space-x-3 min-w-max">
                 <CgWebsite className='text-2xl' />
-                <span>Company website</span>
+                <span>{details.companyWebsite}</span>
             </div>
-            <div className="salary h-8 w-full  flex items-center space-x-3">
+            <div className="salary h-8 w-full  flex items-center space-x-3 min-w-max">
                 <div className="salary__icon">
                     <BsCash className='text-2xl' />
                 </div>
                 <div className="salary">
-                    100,000 - 200,000
+                    <span>{details.salaryRange}</span>
                 </div>
             </div>
         </div>
